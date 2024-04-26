96f966ff03f606413d8bbe152682ee1c27cdbd67
Diegorem <diegorem99@gmail.com>
2024-04-26 11:43:37 -0600

    Creacion de la UI del Doctor

--- src/model/Doctor.java
+++ src/model/Doctor.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -20,7 +22,7 @@ public class Doctor extends User{
     }
 
     ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
-    public void addAvailableAppointment(Date date, String time){
+    public void addAvailableAppointment(String date, String time){
         availableAppointments.add(new AvailableAppointment(date, time));
     }
 
@@ -45,9 +47,14 @@ public class Doctor extends User{
         private int id;
         private Date date;
         private String time;
+        SimpleDateFormat format = new SimpleDateFormat("dd/mm/yyyy");
 
-        public AvailableAppointment(Date date, String time){
-            this.date = date;
+        public AvailableAppointment(String date, String time){
+            try {
+                this.date = format.parse(date);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
             this.time = time;
         }
 
@@ -62,6 +69,11 @@ public class Doctor extends User{
         public Date getDate() {
             return date;
         }
+
+        public String getDate(String DATE) {
+            return format.format(date);
+        }
+
 
         public void setDate(Date date) {
             this.date = date;
